721ba09eaf55a55d0674bac35727e4ae5c294b36
MariaHokas <maria_ahokas@yahoo.com>
2021-08-21 15:50:02 +0300

    Added data sorting

--- src/Components/FilmContent/FilmContent.js
+++ src/Components/FilmContent/FilmContent.js
@@ -2,8 +2,10 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import { Button } from "@material-ui/core";
 
 import { FilmItem } from "../FilmItem";
+import { useSortableData } from "../../Helpers";
 
 const useStyles = makeStyles({
   root: {
@@ -23,14 +25,35 @@ const useStyles = makeStyles({
 });
 
 export const FilmContent = ({ data }) => {
-  console.log("täm,ä on data", data);
   const classes = useStyles();
-
+  const { items, requestSort, sortConfig } = useSortableData(data.films);
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
   return (
     <Container className={classes.root}>
+      <Button
+        type="button"
+        onClick={() => requestSort("Year")}
+        className={getClassNamesFor("Year")}
+      >
+        Year
+      </Button>
+
+      <Button
+        color="primary"
+        type="button"
+        onClick={() => requestSort("Title")}
+        className={getClassNamesFor("Title")}
+      >
+        Title
+      </Button>
       <div>
         <Grid container spacing={3}>
-          {data.films.map((item) => (
+          {items.map((item) => (
             <Grid item xs={12} md={6} lg={3} zeroMinWidth key={item.imdbID}>
               <FilmItem film={item} />
             </Grid>
