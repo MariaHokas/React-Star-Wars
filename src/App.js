61fe7ec8aaa411afd081cd0fdc0ff25d58992755
MariaHokas <maria_ahokas@yahoo.com>
2021-08-21 15:37:47 +0300

    Added UI Material Theme and font

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { Header } from "./Components/Header/";
 import { FilmsPage } from "./Pages";
 const queryClient = new QueryClient();
+const theme = createTheme({
+  typography: {
+    fontFamily: ["BenchNine", "sans-serif"].join(","),
+  },
+  palette: {
+    primary: {
+      main: "#ff0000",
+      "&:hover": {
+        backgroundColor: "#8F00FF",
+      },
+    },
+    secondary: {
+      main: "#8F00FF",
+    },
+  },
+});
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <Header />
-        <FilmsPage />
-      </QueryClientProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          <FilmsPage />
+        </QueryClientProvider>
+      </ThemeProvider>
     </>
   );
 }
