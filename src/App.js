18631267177a2405bca93fa3cae7b3901a965ec6
Maria <maria_ahokas@yahoo.com>
2021-08-23 16:09:56 +0300

    Moved Header component from app.js to FilmsPage.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider, createTheme } from "@material-ui/core/styles";
-import { Header } from "./Components/Header/";
+import { ThemeProvider } from "@material-ui/core/styles";
+
 import { FilmsPage } from "./Pages";
 import { theme } from "./Helpers";
 const queryClient = new QueryClient();
@@ -11,7 +11,6 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <QueryClientProvider client={queryClient}>
-          <Header />
           <FilmsPage />
         </QueryClientProvider>
       </ThemeProvider>
--- src/Pages/FilmsPage.js
+++ src/Pages/FilmsPage.js
@@ -3,10 +3,14 @@ import { useQuery } from "react-query";
 import { FilmContent } from "../Components/FilmContent/index.js";
 import { fetchFilms } from "../Service";
 
+import { Header } from "../Components/Header";
+
 export const FilmsPage = () => {
   const { data, status } = useQuery("films", fetchFilms);
   return (
     <div>
+      <Header />
+
       {status === "loading" && <div>Loading data</div>}
 
       {status === "error" && <div>Error fetching data</div>}
